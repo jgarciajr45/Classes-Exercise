bc4c814b787789084cf2f6c70d1d53f782714773
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-12 07:42:24 -0700

    remove extra car class

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -3,9 +3,12 @@ namespace Classes
 {
 	public class Car
 	{
-		public Car()
-		{
-		}
-	}
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
 }
 
